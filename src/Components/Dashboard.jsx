071634a01bb02625043bc31f67072036a0e05067
KM Adnan Absar <mdadnanjilan1234@gmail.com>
2025-01-25 12:23:33 +0600

    seventh-commit

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,8 +1,14 @@
 
+import {Helmet} from "react-helmet";
 
 const Dashboard = () => {
     return (
         <div>
+             <Helmet>
+                <meta charSet="utf-8" />
+                <title>Dashboard | Winter</title>
+              
+            </Helmet>
             <h2>This is dashboard</h2>
         </div>
     );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,11 +3,16 @@ import About from "./About";
 import Work from "./work";
 import DonationCampaign from "./DonationCampaign";
 import Login from "./Login";
-
+import {Helmet} from "react-helmet";
 const Home = () => {
     return (
 
 <div>
+<Helmet>
+                <meta charSet="utf-8" />
+                <title>Home | Winter</title>
+               
+            </Helmet>
 <div className="mb-8" >
 <Banner></Banner>
 
